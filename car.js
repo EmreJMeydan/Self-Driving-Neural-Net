57ad80fb193de34a7087cf5130df3d260f5793ab
EJM <ej.meydan@outlook.com>
2026-09-14 02:23:52 +0100

    Car controls expnded and movement includes angled movement

--- car.js
+++ car.js
@@ -5,17 +5,69 @@ class Car{
         this.width=width;
         this.height=height;
 
+        this.speed =0;
+        this.acceleration=0.2;
+        this.max_speed=3;
+        this.friction=0.05;
+        this.angle=0;
+
         this.controls = new Controls();
     }
 
+    update(){
+        if(this.controls.forward){
+            this.speed += this.acceleration;
+        }
+        if(this.controls.reverse){
+            this.speed -= this.acceleration;
+        }
+
+        if(this.speed > this.max_speed){
+            this.speed = this.max_speed;
+        }
+
+        if(this.speed < -this.max_speed/2){
+            this.speed = -this.max_speed/2;
+        }
+
+        if(this.speed>0){
+            this.speed-=this.friction;
+        }
+
+        if(this.speed<0){
+            this.speed+=this.friction;
+        }
+
+        if(Math.abs(this.speed)<this.friction){
+            this.speed =0;
+        }
+
+        if(this.controls.left){
+            this.angle += 0.03;
+        }
+
+        if(this.controls.right){
+            this.angle -= 0.03;
+        }
+
+        this.x-= Math.sin(this.angle)*this.speed;
+        this.y-=Math.cos(this.angle)*this.speed;
+    }
+
     draw(ctx){
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(-this.angle);
+
         ctx.beginPath();
         ctx.rect(
-            this.x-this.width/2,
-            this.y-this.width/2,
+            -this.width/2,
+            -this.width/2,
             this.width,
             this.height
         );
         ctx.fill();
+
+        ctx.restore();
     }
 }
